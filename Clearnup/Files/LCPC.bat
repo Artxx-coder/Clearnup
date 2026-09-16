44f66be4bffe2f614518509382fa869396fd8b7f
arturbastoscanellascarvalho-ops <arturbastoscanellascarvalho@gmail.com>
2026-09-15 21:48:02 -0300

    Add files via upload

---
+++ Clearnup/Files/LCPC.bat
@@ -0,0 +1,34 @@
+@echo off
+color 06
+Title Limpeza Completa do PC
+
+set "windows=%windir%"
+set "systemdrive=%systemdrive%"
+set "userprofile=%userprofile%"
+set "temp=%temp%"
+set "history=%userprofile%\Local Settings\History"
+set "cookies=%userprofile%\Cookies"
+set "recent=%userprofile%\Recent"
+set "printers=%systemroot%\system32\spool\printers"
+
+del /s /f /q "%windows%\temp\*.*" 2>nul
+del /s /f /q "%windows%\Prefetch\*.exe" 2>nul
+del /s /f /q "%windows%\Prefetch\*.dll" 2>nul
+del /s /f /q "%windows%\Prefetch\*.pf" 2>nul
+del /s /f /q "%windows%\system32\dllcache\*.*" 2>nul
+del /s /f /q "%systemdrive%\Temp\*.*" 2>nul
+del /s /f /q "%temp%\*.*" 2>nul
+del /s /f /q "%history%\*.*" 2>nul
+del /s /f /q "%userprofile%\Local Settings\Temporary Internet Files\*.*" 2>nul
+del /s /f /q "%userprofile%\Local Settings\Temp\*.*" 2>nul
+del /s /f /q "%recent%\*.*" 2>nul
+del /s /f /q "%cookies%\*.*" 2>nul
+
+echo Limpando registros de eventos...
+FOR /F "tokens=*" %%G in ('wevtutil.exe el') DO (call :do_clear "%%G")
+
+:do_clear
+echo Limpando %1...
+wevtutil.exe cl %1
+
+exit
